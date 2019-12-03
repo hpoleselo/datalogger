8ecc2c75ce24238b79e70a55b9dc674560e943a1
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2019-12-02 22:01:10 -0300

    modifications on teste2.py

--- adc/teste2.py
+++ adc/teste2.py
@@ -9,24 +9,16 @@ port1 = "/dev/ttyACM0"
 port2 = "/dev/ttyUSB0"
 
 if (glob.glob(port1)==[port1]): 
-	comport = serial.Serial(port1, 9600)
+	comport = serial.Serial(port1, 9600, timeout=1)
 else: 
-	comport = serial.Serial(port2, 9600)
+	comport = serial.Serial(port2, 9600, timeout=1)
 
-#comport = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) # Setando timeout 1s para a conexao
- 
 PARAM_CARACTER='t'
-PARAM_ASCII=str(chr(116))       # Equivalente 116 = t
- 
-# Time entre a conexao serial e o tempo para escrever (enviar algo)
-time.sleep(1.8) # Entre 1.5s a 2s
- 
-#comport.write(PARAM_CARACTER)
-comport.write(PARAM_ASCII.encode())
- 
-VALUE_SERIAL=comport.readline()
-
-print ('\nRetorno da serial: {!r}'.format(VALUE_SERIAL))
+time.sleep(1.8)
+for i in range(100):
+	comport.write(PARAM_CARACTER.encode())
+	VALUE_SERIAL=int.from_bytes(comport.read(), "big")
+	print ('\nRetorno da serial: {!r}'.format(VALUE_SERIAL))
  
 # Fechando conexao serial
 comport.close()
